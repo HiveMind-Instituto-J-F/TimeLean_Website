c0b8345483cfb51711588c111925516136a7ade7
Quitto <brunamoreiraquitto@gmail.com>
2025-08-27 14:39:33 -0300

    Fix Servelts and add web

--- src/main/java/hivemind/hivemindweb/Servelets/AboutUsServelet.java
+++ src/main/java/hivemind/hivemindweb/Servelets/AboutUsServelet.java
@@ -7,12 +7,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/about_us")
+@WebServlet("/about")
 public class AboutUsServelet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            req.getRequestDispatcher("aboutus.jsp").forward(req,resp);
+            req.getRequestDispatcher("html\\about-us.html").forward(req,resp);
         }catch (ServletException se){
             se.printStackTrace();
         }
