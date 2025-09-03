71bbdcc41789939c700e20a83d0e063ab5bbb679
amandaandrade9410 <amanda.paraaulas@gmail.com>
2025-09-03 08:01:17 -0300

    Classe Planos pronta

---
+++ Company.java
@@ -0,0 +1,10 @@
+public class Company {
+    // Variáveis
+    private long CNPJ;
+    private String companyName;
+    private String companyType;
+    private String registrantName;
+    private String registrantLastName;
+    private
+
+}
---
+++ Plans.java
@@ -0,0 +1,61 @@
+public class Plans {
+    // Variáveis
+    private int id;
+    private String name;
+    private String description;
+    private int qntSales;
+    private int duration;
+    private  double value;
+
+    // Construtor
+    public Plans(int id, String name, String description, int qntSales, int duration, double value){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.qntSales = qntSales;
+        this.duration = duration;
+        this.value = value;
+    }
+
+    // ToString
+    public String toString(){
+        return "ID: "+this.id+"\nNome: "+this.name+"\nDescrição: "+this.description+"\nQuantidade de Vendas: "+qntSales
+                +"\nDuração: "+this.duration+ format("Valor: %.2f", this.value);
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getQntSales() {
+        return qntSales;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public double getValue() {
+        return value;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setQntSales(int qntSales) {
+        this.qntSales = qntSales;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
+
