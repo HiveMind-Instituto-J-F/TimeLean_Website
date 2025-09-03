45271183a3c4c3decf81a9051fd80413331e547e
Quitto <brunamoreiraquitto@gmail.com>
2025-09-03 10:54:52 -0300

    Add models wihtout links in DAO

---
+++ src/main/java/hivemind/hivemindweb/models/Plans.java
@@ -0,0 +1,63 @@
+package hivemind.hivemindweb.models;
+
+public class Plans {
+    // Variáveis
+    private int id;
+    private String name;
+    private String description;
+    private int qntSales;
+    private int duration;
+    private  double value;
+
+    // Construtor
+    public Plans(int id, String name, String description, int qntSales, int duration, double value){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.qntSales = qntSales;
+        this.duration = duration;
+        this.value = value;
+    }
+
+    // ToString
+    public String toString(){
+        return "ID: "+this.id+"\nName: "+this.name+"\nDescription: "+this.description+"\nQuantity of Sales: "+qntSales
+                +"\nDuration: "+this.duration+ ("Value: %.2f," +  this.value);
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getQntSales() {
+        return qntSales;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public double getValue() {
+        return value;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setQntSales(int qntSales) {
+        this.qntSales = qntSales;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
+
---
+++ src/main/java/hivemind/hivemindweb/models/Plant.java
@@ -0,0 +1,106 @@
+package hivemind.hivemindweb.models;
+
+public class Plant {
+    // Variáveis
+    private long CNAE;
+    private long CNPJ;
+    private String location;
+    private String responsible;
+    private String operationalStatus;
+    private String state;
+    private String city;
+    private String street;
+    private String country;
+    private String adressType;
+    private int number;
+    private long CEP;
+
+    // Construtor
+    public Plant(long CNAE, long CNPJ, String location, String responsible, String operationalStatus, String state, String city, String street,
+                 String country, String adressType, int number, long CEP){
+        this.CNAE = CNAE;
+        this.CNPJ = CNPJ;
+        this.location = location;
+        this.responsible = responsible;
+        this.operationalStatus = operationalStatus;
+        this.state = state;
+        this.city = city;
+        this.street = street;
+        this.country = country;
+        this.adressType = adressType;
+        this.number = number;
+        this.CEP = CEP;
+    }
+
+    // ToString
+    public String toString(){
+        return "CNAE: "+this.CNAE+"\nCNPJ: "+this.CNPJ+"\nLocation: "+this.location+"\nResponsible: "+this.responsible+"\nOperational Status: "
+                +this.operationalStatus+"\nCountry: "+this.country+"\nState: "+this.state+"\nCity: "+this.city+"\nStreet: "+this.street
+                +"\nNumber: "+this.number+"\nCEP: "+this.CEP;
+    }
+
+    // Getters e Setters
+    public long getCNAE() {
+        return CNAE;
+    }
+    public long getCNPJ() {
+        return CNPJ;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public String getResponsible() {
+        return responsible;
+    }
+    public String getOperationalStatus() {
+        return operationalStatus;
+    }
+    public String getState() {
+        return state;
+    }
+    public String getCity() {
+        return city;
+    }
+    public String getStreet() {
+        return street;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public String getAdressType() {
+        return adressType;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public long getCEP() {
+        return CEP;
+    }
+    public void setResponsible(String responsible) {
+        this.responsible = responsible;
+    }
+    public void setOperationalStatus(String operationalStatus) {
+        this.operationalStatus = operationalStatus;
+    }
+    public void setCEP(long CEP) {
+        this.CEP = CEP;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    public void setAdressType(String adressType) {
+        this.adressType = adressType;
+    }
+}
