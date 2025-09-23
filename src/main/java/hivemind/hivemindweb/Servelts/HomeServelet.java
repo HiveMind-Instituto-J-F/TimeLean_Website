5b4691b8c7d0fe238b91b828d208759daa66be6f
Quitto <brunamoreiraquitto@gmail.com>
2025-09-23 17:31:12 -0300

    Add Front And strart link i back

--- src/main/java/hivemind/hivemindweb/Servelts/HomeServelet.java
+++ src/main/java/hivemind/hivemindweb/Servelts/HomeServelet.java
@@ -14,7 +14,7 @@ public class HomeServelet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            req.getRequestDispatcher("index.html").forward(req,resp);
+            req.getRequestDispatcher("\\index.html").forward(req,resp);
         }catch (ServletException se){
             se.printStackTrace();
         }
