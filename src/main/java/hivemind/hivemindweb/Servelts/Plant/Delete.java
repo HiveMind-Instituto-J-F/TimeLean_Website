75f5a1b763bd473d553095e27b99c5a50a7c2811
Eduardo Costa <eduardocosta942z@gmail.com>
2025-10-18 00:10:24 -0300

    maint: business rules

--- src/main/java/hivemind/hivemindweb/Servelts/Plant/Delete.java
+++ src/main/java/hivemind/hivemindweb/Servelts/Plant/Delete.java
@@ -10,11 +10,19 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/delete-plant")
+@WebServlet("/plant/delete")
 public class Delete extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         String plant_cnpj = request.getParameter("cnpj");
-        PlantDAO.delete(new Plant(plant_cnpj));
-        response.sendRedirect("html/crud/plant/read.jsp");
+
+        Plant plant = new Plant(plant_cnpj);
+        plant.setOperationalStatus(false);
+        PlantDAO.switchOperationalStatus(plant);
+        response.sendRedirect(request.getContextPath() + "/plant/read");
     }
 }
+/*
+ * BUSINESS RULES (DO NOT DELETE)
+ * In order to rollback, the system must verify if company is active.
+ * Workers must not be deactivated/deleted after a plant deactivation/deletion.
+ */
